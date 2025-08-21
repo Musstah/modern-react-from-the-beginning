89190b4e9a23a47a3406e7886c454da8def23b94
Musstah <pythonapilearn@gmail.com>
2025-08-21 20:45:31 +0200

    Cleaned .gitignore and restructured frontend/backend

---
+++ friendly-dev/friendly-dev-frontend/app/routes/blog/index.tsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import type { Route } from "./+types";
+import type { Post, StrapiResponse, StrapiPost } from "~/types";
+import PostCard from "~/components/PostCard";
+import Pagination from "~/components/Pagination";
+import PostFilter from "~/components/PostFilter";
+
+export async function loader({ request }: Route.LoaderArgs): Promise<{ posts: Post[] }> {
+    const res = await fetch(`${import.meta.env.VITE_API_URL}/posts?populate=image&sort=date:desc`)
+
+    if (!res.ok) throw new Error('Failed to fetch data')
+
+    const json: StrapiResponse<StrapiPost> = await res.json()
+
+    const posts = json.data.map((item) => ({
+        id: item.id,
+        title: item.title,
+        excerpt: item.excerpt,
+        slug: item.slug,
+        date: item.date,
+        body: item.body,
+        image: item.image?.url ? `${import.meta.env.VITE_STRAPI_URL}${item.image.url}` : '/images/no-image.png',
+    }))
+
+
+    return { posts }
+}
+
+const BlogPage = ({ loaderData }: Route.ComponentProps) => {
+
+    const [searchQuery, setSearchQuery] = useState('')
+    const [currentPage, setCUrrentPage] = useState(1)
+    const postsPerPage = 10;
+
+
+    const { posts } = loaderData;
+
+    const filteredPosts = posts.filter((post) => {
+        const query = searchQuery.toLowerCase();
+        return (
+            post.title.toLowerCase().includes(query) ||
+            post.excerpt.toLowerCase().includes(query)
+        )
+    })
+    const totalPages = Math.ceil(filteredPosts.length / postsPerPage)
+    const indexOfLast = currentPage * postsPerPage
+    const indexOfFirst = indexOfLast - postsPerPage
+    const currentPost = filteredPosts.slice(indexOfFirst, indexOfLast)
+
+    return (
+        <div className="max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900">
+            <h2 className='text-3xl font-bold mb-8 text-white'>📝 Blog</h2>
+
+            <PostFilter searchQuery={searchQuery} onSearchChange={(query) => {
+                setSearchQuery(query)
+                setCUrrentPage(1)
+            }} />
+            <div className="space-y-8">
+                {currentPost.length === 0 ? (
+                    <p className="text-gray-400 text-center">
+                        No posts found
+                    </p>
+                ) : (currentPost.map((post) => (
+                    <PostCard key={post.slug} post={post} />
+                )))}
+            </div>
+
+
+            {totalPages > 1 && <Pagination
+                currentPage={currentPage}
+                totalPages={totalPages}
+                onPageChange={(page) => setCUrrentPage(page)}
+            />}
+
+        </div>
+    );
+};
+
+export default BlogPage;
